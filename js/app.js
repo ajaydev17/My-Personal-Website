d800e3c0adbef29c4d5427b307f55ffa18051a94
Ajay Dev <devadigaajay1729@gmail.com>
2022-07-20 22:04:07 +0530

    feat: added javascript code for navbar
    feat: added javascript code for navbar

---
+++ js/app.js
@@ -0,0 +1,8 @@
+// event listener for the checkbox click
+window.addEventListener("scroll", function () {
+    let openNavMenu = document.getElementById('navi-toggle').checked;
+
+    if (openNavMenu = true) {
+        document.getElementById('navi-toggle').checked = false;
+    }
+});
